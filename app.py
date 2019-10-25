502816d114cba7384c8fa9200e53f7df8d5572f9
Vishnu Prakash <stanbrooks07@gmail.com>
2019-10-25 22:40:55 +0530

    Go to '/' only after adding items

--- app.py
+++ app.py
@@ -1,11 +1,43 @@
-from flask import Flask,url_for
-from flask import render_template
+from datetime import datetime
+from flask import Flask,request,jsonify,render_template,redirect,url_for
+from elasticsearch import Elasticsearch
 
 app=Flask(__name__)
+es = Elasticsearch()
 
-@app.route('/')
-def hello_world():
+#Inserting new items
+@app.route('/api',methods=['GET','POST'])
+def search():
+    if request.method=='POST':
+        #author=request.form['author']
+        text=request.form['text']
+
+        body={
+            'text':text,
+            'author':"ninja",
+            'timestamp':datetime.now()
+        }
+        result=es.index(index="search-index",doc_type='url',id=1,body=body)
+        return redirect(url_for('search_result',text=text))
+    #es.indices.refresh(index="search-index")
     return render_template('index.html')
+
+#showing the content
+@app.route('/', methods=['GET'])
+def index():
+    if request.method=='GET':
+        #return "Results"
+        res = es.get(index="search-index", doc_type='url', id=1)
+        return jsonify(res['_source'])
+    return "neigh"
+
+#Search results
+@app.route('/result')
+def search_result():
+    text = request.args.get('text', None)
+    return text
+    
+
 
 if __name__ == "__main__":
     app.run(debug=True)
